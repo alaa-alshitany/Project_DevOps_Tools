8018fe58067e15c6ef3000b587e1b3f5ea250055
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-08 18:11:19 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
    backend "s3" {
      bucket         = "lab2-env-bk" 
      key            = "terraform.tfstate" 
-     region         = "eu-west-1"
+     region         = "eu-west-2"
      encrypt        = true
      dynamodb_table = "terraform-lock-dev"
    }
