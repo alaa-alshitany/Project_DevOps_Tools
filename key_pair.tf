80f78d2a2cf0dcbbc4d1dbe6bd9efff4939afa9e
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-08 21:03:41 +0300

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -4,13 +4,13 @@ resource "tls_private_key" "ssh_key" {
   rsa_bits  = 2048  
 }
 //save private key to s3 bucket
-resource "aws_s3_bucket_object" "private_key" {
+resource "aws_s3_object" "private_key" {
   bucket = "lab2-env-bk "
   key    = "private_key.pem"
   content  = tls_private_key.ssh_key.private_key_pem   
 }
 //upload public key to s3 bucket 
-resource "aws_s3_bucket_object" "public_key" {
+resource "aws_s3_object" "public_key" {
   bucket = "lab2-env-bk "
   key_name   = "public_key.pub"  
   public_key = tls_private_key.ssh_key.public_key_openssh 
