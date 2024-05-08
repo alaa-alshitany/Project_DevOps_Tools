830c00b59f0a7c499bbb1e60bceb4473700608ea
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-08 19:32:50 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
      key            = "terraform.tfstate" 
      region         = "eu-west-2"
      encrypt        = true
-     dynamodb_table = "terraform-lock-dev"
+     #dynamodb_table = "terraform-lock-dev"
    }
 }
