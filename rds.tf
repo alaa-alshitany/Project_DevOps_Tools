5d59c4cb65b36c6851c5fbb62ef83eac7fe2050d
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-09 09:27:16 +0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "database_subnet_group" {
   name       = "cloud_pd44_database_subnet_group"
-  subnet_ids = [module.network_module.subnets["private_subnet_I"].id, module.network_module.subnets["private_subnet_II"].id]
+  subnet_ids = [module.network_module.subnets["private_subnet_1"].id, module.network_module.subnets["private_subnet_2"].id]
   
   tags = {
     Name = "${var.common_resource_name}_database_Subnet_Group"
