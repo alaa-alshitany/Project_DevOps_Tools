64dd8ed3a0b7394454d89686e49c16994ce6b548
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-09 09:22:48 +0300

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -1,19 +1,21 @@
-//tls generate 2 keys (public , private)
+// TLS generate 2 keys (public, private)
 resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA" 
-  rsa_bits  = 2048  
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
-//save private key to s3 bucket
+
+// Save private key to S3 bucket
 resource "aws_s3_object" "private_key" {
-  bucket = "lab2-env-bk "
+  bucket = "lab2-env-bk"
   key    = "private_key.pem"
-  content  = tls_private_key.ssh_key.private_key_pem   
+  content  = tls_private_key.ssh_key.private_key_pem
 }
-//upload public key to s3 bucket 
+
+// Upload public key to S3 bucket
 resource "aws_s3_object" "public_key" {
-  bucket = "lab2-env-bk "
-  key_name   = "public_key.pub"  
-  public_key = tls_private_key.ssh_key.public_key_openssh 
+  bucket = "lab2-env-bk"
+  key    = "public_key.pub" 
+  content = tls_private_key.ssh_key.public_key_openssh  
 }
 
 resource "aws_key_pair" "ssh_key" {
