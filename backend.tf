3306c42bb69ba8fa36a63e169ddad62862555149
alaa alshitany <alaa2alshitany@gmail.com>
2024-05-09 10:35:07 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket         = "simple-project-bk" 
+     bucket         = "simple-proj-bk" 
      key            = "terraform.tfstate" 
      region         = "eu-west-2"
      encrypt        = true
