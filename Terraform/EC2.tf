fcccc63a02f1dfe89b3ea363c85ee1d39b507f50
alaaWalid <alaa2alshitany@gmail.com>
2024-05-20 02:03:28 +0300

    make the machines ubuntu

--- Terraform/EC2.tf
+++ Terraform/EC2.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_instance" "bastion" {
-  ami           = "ami-008ea0202116dbc56" 
+  ami           = "ami-053a617c6207ecc7b"
   instance_type = var.machine_data["type"]    
   subnet_id     = module.network_module.subnets["public_subnet_1"].id 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
@@ -14,16 +14,15 @@ resource "aws_instance" "bastion" {
   }
   user_data = <<-EOF
     #!/bin/bash
-    mkdir -p /home/ec2-user/.ssh
-    echo "${local.ssh_private_key}" > /home/ec2-user/.ssh/id_rsa
-    chmod 600 /home/ec2-user/.ssh/id_rsa
-    chown ec2-user:ec2-user /home/ec2-user/.ssh/id_rsa
+    sudo mkdir -p /home/ubuntu/.ssh
+    echo "${local.ssh_private_key}" | sudo tee /home/ubuntu/.ssh/id_rsa
+    sudo chmod 600 /home/ubuntu/.ssh/id_rsa
+    sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
   EOF
-
 }
 
 resource "aws_instance" "application" {
-  ami           = "ami-008ea0202116dbc56"  
+  ami           = "ami-053a617c6207ecc7b"
   instance_type = var.machine_data["type"]    
   subnet_id     = module.network_module.subnets["private_subnet_1"].id  
   vpc_security_group_ids = [aws_security_group.allow_ssh_and_3000.id]
