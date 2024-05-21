cd703b0ce74b645266a273346085534f957a7a67
alaaWalid <alaa2alshitany@gmail.com>
2024-05-21 09:06:05 +0300

    .

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
    backend "s3" {
      bucket         = "simple-project-bk" 
      key            = "terraform.tfstate" 
-     region         = "eu-west-2"
+     region         = "eu-west-1"
      encrypt        = true
      dynamodb_table = "simple-proj-tb"
    }
