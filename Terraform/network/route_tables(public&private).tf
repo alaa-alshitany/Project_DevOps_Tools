d33153ab7fcb38d0c005877ead41c7a4b309f7b2
alaaWalid <alaa2alshitany@gmail.com>
2024-05-22 11:49:01 +0300

    .

--- Terraform/network/route_tables(public&private).tf
+++ Terraform/network/route_tables(public&private).tf
@@ -1,22 +1,40 @@
-resource "aws_route_table" "RT" {
-  count=2
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = count.index == 0? aws_internet_gateway.igw.id:aws_nat_gateway.ngw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
+
   tags = {
-    Name = "${var.common_resource_name}_RT"
+    Name = "public_RT"
   }
 }
 
-resource "aws_route_table_association" "pub_rt_associate" {
-  subnet_id      = aws_subnet.subnets["public_subnet_1"].id
-  route_table_id = aws_route_table.RT[0].id
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "private_RT"
+  }
 }
 
 
-resource "aws_route_table_association" "priv_rt_associate" {
+
+resource "aws_route_table_association" "public_rt_associations" {
+  count          = 2
+  subnet_id      = aws_subnet.subnets["public_subnet_1"].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_rt_associations" {
+  count          = 2
   subnet_id      = aws_subnet.subnets["private_subnet_1"].id
-  route_table_id = aws_route_table.RT[1].id
+  route_table_id = aws_route_table.private_route_table.id
 }
