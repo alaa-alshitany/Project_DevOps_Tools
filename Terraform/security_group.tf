b80d5e3f61b66b1dca1f70f4e3618c7808c81afc
alaaWalid <alaa2alshitany@gmail.com>
2024-05-22 12:46:51 +0300

    .

--- Terraform/security_group.tf
+++ Terraform/security_group.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "allow_ssh_and_3000" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]  
+    cidr_blocks     = ["10.0.0.0/16"]  
   }
 
   ingress {
