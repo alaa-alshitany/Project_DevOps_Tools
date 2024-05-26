d865b402d4d6e126c564c754fd76c0e943018885
alaaWalid <alaa2alshitany@gmail.com>
2024-05-26 15:34:56 +0300

    updated-sg-for rds&redis

--- Terraform/security_group.tf
+++ Terraform/security_group.tf
@@ -53,4 +53,40 @@ resource "aws_security_group" "allow_ssh_and_3000" {
   }
 }
 
+resource "aws_security_group" "redis_sg" {
+  name          = "redis-sg"
+  vpc_id        = module.network_module.vpc_id
 
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name          = "rds-sg"
+  vpc_id        = module.network_module.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
