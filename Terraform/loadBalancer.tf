be4967b4869250ad41a5cb4620c62f70cb6b874c
alaaWalid <alaa2alshitany@gmail.com>
2024-05-26 17:53:20 +0300

    Added ALB with it's security group

---
+++ Terraform/loadBalancer.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "ALB" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg-alb.id]
+  subnets            = [module.network_module.subnets["public_subnet_1"].id, module.network_module.subnets["public_subnet_2"].id]
+
+  tags = {
+    Name = "Nodejs Alb"
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "alb-target-group"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id = module.network.vpc_id
+
+  tags = {
+    Name = "Alb Target Group"
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn  = aws_lb.ALB.arn
+  port               = 80
+  protocol           = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "alb_target_group_attachment" {
+  target_group_arn  = aws_lb_target_group.alb_target_group.arn
+  target_id         = aws_instance.application.id
+  port              = 3000
+}
--- Terraform/security_group.tf
+++ Terraform/security_group.tf
@@ -90,3 +90,25 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "sg-alb" {
+  vpc_id        = module.network_module.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-alb"
+  }
+}
